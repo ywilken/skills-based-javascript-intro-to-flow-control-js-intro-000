8093bace5cce6ffb99e10f3a416229b7d445c8fe
Yannick Wilken <yannickwilken99@gmail.com>
2020-04-04 13:51:19 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,10 +28,10 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case age>=13 && age<=19:
-      return "You are a teenager"
+    case >=13 && <=19:
+      return "You are a teenager";
       break;
     default:
-      return "You have an age"
+      return "You have an age";
   }
 }
